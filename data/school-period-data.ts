c8bce20c2b210727506320c1f4b160c15a5c5b04
DrrylPns <darrylpaniss@gmail.com>
2024-10-09 06:42:00 +0800

    Add school-periods form and table

---
+++ data/school-period-data.ts
@@ -0,0 +1,78 @@
+export const schoolPeriodData = [
+    {
+        id: "1",
+        period: "1st Period",
+        timeRange: "6AM - 7AM",
+    },
+    {
+        id: "2",
+        period: "2nd Period",
+        timeRange: "7AM - 8AM",
+    },
+    {
+        id: "3",
+        period: "3rd Period",
+        timeRange: "8AM - 9AM",
+    },
+    {
+        id: "4",
+        period: "4th Period",
+        timeRange: "9AM - 10AM",
+    },
+    {
+        id: "5",
+        period: "5th Period",
+        timeRange: "10AM - 11AM",
+    },
+    {
+        id: "6",
+        period: "6th Period",
+        timeRange: "11AM - 12PM",
+    },
+    {
+        id: "7",
+        period: "7th Period",
+        timeRange: "12PM - 1PM",
+    },
+    {
+        id: "8",
+        period: "8th Period",
+        timeRange: "1PM - 2PM",
+    },
+    {
+        id: "9",
+        period: "9th Period",
+        timeRange: "2PM - 3PM",
+    },
+    {
+        id: "10",
+        period: "10th Period",
+        timeRange: "3PM - 4PM",
+    },
+    {
+        id: "11",
+        period: "11th Period",
+        timeRange: "4PM - 5PM",
+    },
+    {
+        id: "12",
+        period: "12th Period",
+        timeRange: "5PM - 6PM",
+    },
+    {
+        id: "13",
+        period: "13th Period",
+        timeRange: "6PM - 7PM",
+    },
+]
+
+export const schoolPeriodColumns = [
+    {
+        accessorKey: "period",
+        header: "Period",
+    },
+    {
+        accessorKey: "timeRange",
+        header: "Time Range",
+    },
+]
--- src/app/(system-admin)/(sysadmin-school)/sysadmin-school-period/_components/add-school-period-card.tsx
+++ src/app/(system-admin)/(sysadmin-school)/sysadmin-school-period/_components/add-school-period-card.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { DatePickerWithRange } from "@/components/date-range-picker"
 import { Button } from "@/components/ui/button"
 import {
     Card,
@@ -13,30 +12,36 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
-export const AddSchoolYearCard = () => {
+export const AddSchoolPeriodCard = () => {
     return (
         <Card className="flex flex-col h-fit">
             <CardHeader>
-                <CardTitle>Add School Year</CardTitle>
+                <CardTitle>Add School Period</CardTitle>
                 <CardDescription>
-                    Fill out the form to add a school year
+                    Fill out the form to add a school period
                 </CardDescription>
             </CardHeader>
             <CardContent className="grid gap-8 mt-7">
                 <div className="grid gap-2">
-                    <Label htmlFor="batchName" className="font-semibold">Batch Name</Label>
+                    <Label htmlFor="period">Period</Label>
                     <Input
-                        id="batchName"
+                        id="period"
                         type="text"
-                        placeholder="Batch name"
+                        placeholder="Ex: 1st Period"
                         className="w-full"
                     />
                 </div>
 
                 <div className="grid gap-2">
-                    <Label htmlFor="SY" className="font-semibold">Start Date - End Date</Label>
-                    <DatePickerWithRange />
+                    <Label htmlFor="tr" className="font-semibold">Time Range</Label>
+                    <Input
+                        id="tr"
+                        type="text"
+                        placeholder="Ex: 6:30AM - 7:30AM"
+                        className="w-full"
+                    />
                 </div>
+
             </CardContent>
             <CardFooter className="mt-auto">
                 <Button className="text-white">
